52e44df4db94daad321adac54e68f92b32a5617c
fernandomv3 <fernandomv3@gmail.com>
2023-05-22 09:39:54 -0500

    Fix: Chromium ya no se cierra inesperadamente

--- diagramarPrueba.py
+++ diagramarPrueba.py
@@ -22,7 +22,8 @@ warnings.filterwarnings('ignore', category=UserWarning, module='openpyxl')
 async def get_browser():
   return await launch({
         'executablePath':'/usr/bin/chromium',
-        'headless':True
+        'headless':True,
+        'args':['--no-sandbox','--disable-setuid-sandbox']
       },handleSIGINT=False,
       handleSIGTERM=False,
       handleSIGHUP=False)
